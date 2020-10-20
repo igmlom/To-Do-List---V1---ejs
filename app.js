2b47738ff6c87ae473ff250f1574b1fd3a5cbd9e
igmlom <igmlom210@gmail.com>
2020-10-20 23:48:41 +0300

    add mongoose to save data end lesson 368

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = ["Buy Food","Eat Food"];
-let workItems = [];
+
+mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item ({
+    name: "Welcome!"
+})
+
+const item2 = new Item ({
+    name: "Eat Food"
+})
+const item3 = new Item ({
+    name: "That's important"
+})
+
+const defaultItems = [item1, item2, item3]
+
+
 app.get("/", (req, res) => {
     
-    res.render("list", { listTitle: date.getDate(), items: items });
+
+    Item.find(function(err, foundItem){
+        
+        console.log(foundItem);
+        if(foundItem.length === 0){
+            Item.insertMany(defaultItems, err=>{if(err){console.log(err)}})
+            res.redirect("/");
+        }else{
+            res.render("list", { listTitle: "Today", items: foundItem });
+        }
+
+    })
 })
 
 app.post("/", (req, res)=>{
-    console.log(req.body);
-    var item = req.body.new;
+    
 
-    if(req.body.list === "Work list"){
-            workItems.push(item);
-            res.redirect('/work');
-    }else{
-        var item = req.body.new;
-        items.push(item);
-        res.redirect('/');
-    }
+    const itemName = req.body.newItem;
+
+    const item = new Item({
+        name: itemName
+    })
+
+    item.save();
+
+    
+    res.redirect("/");
 })
+
+app.post("/delete", (req, res)=>{
+    console.log(req.body);
+    const delItemId = req.body.checkbox;
+    Item.deleteOne({_id: delItemId}, err=>console.log(err))
+    res.redirect("/")
+})
+
 
 app.get("/work", (req, res)=> {
     res.render("list", { listTitle: "Work list", items: workItems });
