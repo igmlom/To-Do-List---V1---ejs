e5c45756636bd822063610041e9193cbf38708f4
igmlom <igmlom210@gmail.com>
2020-10-21 13:07:52 +0300

    update

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
-const e = require("express");
 const _ = require("lodash");
+
 
 const app = express();
 app.set("view engine", "ejs");
@@ -17,76 +17,86 @@ const itemSchema = {
     name: String
 };
 
-const Item = mongoose.model("Item", itemSchema)
-
-const item1 = new Item({
-    name: "Welcome!"
-})
-
-const item2 = new Item({
-    name: "Eat Food"
-})
-const item3 = new Item({
-    name: "That's important"
-})
-
-const defaultItems = [item1, item2, item3]
-
-
 const listSchema = {
     name: String,
     items: [itemSchema]
 };
 
+const Item = mongoose.model("Item", itemSchema)
 const List = mongoose.model("List", listSchema);
 
 
+const defaultItems = [
+    new Item({
+        name: "Welcome!"
+    }),
+    
+    new Item({
+        name: "Eat Food"
+    }),
+    new Item({
+        name: "That's important"
+    })
+]
+
+
+
 app.get("/", (req, res) => {
+    const list = new List({
+        name: date.getDate(),
+        items: defaultItems
+    })
 
-
-    Item.find(function (err, foundItem) {
-
-        if (foundItem.length === 0) {
-            Item.insertMany(defaultItems, err => { if (err) { console.log(err) } })
-            res.redirect("/");
-        } else {
-            res.render("list", { listTitle: "Today", items: foundItem });
+    List.findOne({ name:  date.getDate() }, (err, results) => {
+        if (!err) {
+            if (!results) {
+                list.save();
+                res.redirect("/");
+            }
+            else {
+                res.render("list", { listTitle: date.getDate(), items: results.items})
+            }
         }
-
     })
 })
 
 app.post("/", (req, res) => {
 
-
-    const itemName = req.body.newItem;
+    const newItemName = req.body.newItem;
     const listName = req.body.list;
 
     const item = new Item({
-        name: itemName
+        name: newItemName
     })
 
-
-    if (listName === "Today") {
-        item.save();
-        res.redirect("/");
+    if (listName === date.getDate()) {
+        List.findOne({ name: date.getDate() }, (err, foundList) => {
+            if(!err){
+                foundList.items.push(item);
+                foundList.save();
+                res.redirect("/");
+            }
+        })
     } else {
         List.findOne({ name: listName }, (err, foundList) => {
-            foundList.items.push(item);
-            foundList.save();
-            res.redirect("/" + foundList.name);
+            if(!err){
+                foundList.items.push(item);
+                foundList.save();
+                res.redirect("/" + foundList.name);
+            }
         })
     }
 })
+
 
 app.post("/delete", (req, res) => {
     const delItemId = req.body.checkbox;
     const listName = req.body.listName;
 
-    if (listName === "Today") {
-        Item.deleteOne({ _id: delItemId }, err => {
-            if (!err) {
-                res.redirect('/')
+    if (listName === date.getDate()) {
+        List.findOneAndUpdate({name: listName}, {$pull: {items:{_id: delItemId}}}, (err, foundList)=>{
+            if(!err){
+                res.redirect('/');
             }
         })
     } else {
@@ -119,6 +129,7 @@ app.get("/:customListName", (req, res) => {
         }
     })
 })
+
 
 app.listen(8000, () => {
     console.log("server run on 8000");
